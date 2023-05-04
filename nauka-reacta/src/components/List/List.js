f7465fbc1b12cdd1162e69bbf908192b013039d6
wojnoski <karol.wojnarowski@gmail.com>
2023-05-04 18:37:40 +0200

    Add first styles

---
+++ nauka-reacta/src/components/List/List.js
@@ -0,0 +1,23 @@
+const List = () => {
+  return (
+    <div>
+      <header>
+        <h1>Things to do <span>soon</span></h1>
+        <p>Interesting things I want to check out</p>
+      </header>
+      <section>
+        <article>
+          <h2>Books</h2>
+        </article>
+        <article>
+          <h2>Movies</h2>
+        </article>
+        <article>
+          <h2>Games</h2>
+        </article>
+      </section>
+    </div>
+  );
+};
+
+export default List;
--- nauka-reacta/src/index.js
+++ nauka-reacta/src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom";
 import App from './App';
+import './styles/normalize.scss';
+import './styles/global.scss';
 
 ReactDOM.render(
   <App />,
@@ -9,4 +11,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
