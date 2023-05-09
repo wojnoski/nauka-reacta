3460a7fabfaad6c9b7ca402ea37158b5583e9329
wojnoski <karol.wojnarowski@gmail.com>
2023-05-09 20:02:46 +0200

    Add icons

--- nauka-reacta/src/components/Column/Column.js
+++ nauka-reacta/src/components/Column/Column.js
@@ -3,7 +3,7 @@ import styles from './Column.module.scss';
 const Column = props => {
   return (
     <article className={styles.column}>
-      <h2 className={styles.title}>{props.title}</h2>
+      <h2 className={styles.title}><span className={styles.icon + " fa fa-"+ props.icon} ></span>{props.title}</h2>
     </article>
   );
 };
