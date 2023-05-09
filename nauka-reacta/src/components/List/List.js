63722282a482fad0af13955429a32bdc7ec55279
wojnoski <karol.wojnarowski@gmail.com>
2023-05-09 19:53:25 +0200

    Divide into columns

--- nauka-reacta/src/components/List/List.js
+++ nauka-reacta/src/components/List/List.js
@@ -1,4 +1,5 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
 
 const List = () => {
   return (
@@ -8,15 +9,9 @@ const List = () => {
       </header>
       <p className={styles.description}>Interesting things I want to check out</p>
       <section className={styles.columns}>
-        <article>
-          <h2>Books</h2>
-        </article>
-        <article>
-          <h2>Movies</h2>
-        </article>
-        <article>
-          <h2>Games</h2>
-        </article>
+        <Column title="Books" />
+        <Column title="Movies" />
+        <Column title="Games" />
       </section>
     </div>
   );
